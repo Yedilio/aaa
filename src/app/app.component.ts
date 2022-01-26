a583820f573a4de34b3a00e3a8b804d09e55153d
Yedilio <sarsen.yedil@gmail.com>
2022-01-26 23:59:42 +0600

    #fixes new tile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,111 +1,19 @@
 import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(20%)' }),
+        animate('650ms ease-in', style({ transform: 'translateX(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'test-task';
-  tariffs: any[] = [
-    {
-      name: 'Эконом',
-      price_for_km: 4,
-      free_baggage: 5,
-      add_baggage: 4000,
-      max_weight: 20
-    },
-    {
-      name: 'Продвинутый',
-      price_for_km: 8,
-      free_baggage: 20,
-      add_baggage: 5000,
-      max_weight: 50,
-      promo_child: 30,
-      child_age: 7
-    },
-    {
-      name: 'Люкс',
-      price_for_km: 15,
-      free_baggage: 0,
-      max_weight: 50,
-      promo_child: 30,
-      child_age: 16
-    }
-  ];
-  tariffsRJD: any[] = [
-    {
-      name: 'Эконом',
-      price_for_km: 0.5,
-      free_baggage: 15,
-      add_baggage: 50,
-      max_weight: 50,
-      promo_child: 50,
-      child_age: 5
-    },
-    {
-      name: 'Продвинутый',
-      price_for_km: 2,
-      free_baggage: 20,
-      add_baggage: 50,
-      max_weight: 60,
-      promo_child: 30,
-      child_age: 8
-    },
-    {
-      name: 'Люкс',
-      price_for_km: 4,
-      free_baggage: 0,
-      max_weight: 60,
-      promo_child: 20,
-      child_age: 16
-    }
-  ];
-
-  km: number = 0;
-  age: number = 0;
-  weight: number = 0;
-
-  calculate() {
-    this.tariffs.forEach((el) => {
-      if (this.weight > el.max_weight) {
-        el["disabled"] = true;
-      } else {
-        let km_price = this.km * el.price_for_km;
-        el["disabled"] = false;
-        if (el.child_age && this.age < el.child_age) {
-          let sale = km_price * el.promo_child / 100;
-          km_price = km_price - sale;
-        }
-        if (el.free_baggage > 0 && el.add_baggage) {
-          let baggage_price = this.weight > el.free_baggage
-              ? km_price + el.add_baggage : km_price;
-          el.price = baggage_price;
-        } else {
-          el.price = km_price;
-        }
-      }
-    });
-
-    this.tariffsRJD.forEach((el) => {
-      if (this.weight > el.max_weight) {
-        el["disabled"] = true;
-      } else {
-        let km_price = this.km * el.price_for_km;
-        el["disabled"] = false;
-        if (el.child_age && this.age < el.child_age) {
-          let sale = km_price * el.promo_child / 100;
-          km_price = km_price - sale;
-        }
-        if (el.free_baggage > 0 && el.add_baggage) {
-
-          let baggage_price = this.weight > el.free_baggage
-              ? km_price + ((this.weight - el.free_baggage) * el.add_baggage) : km_price;
-          el.price = baggage_price;
-        } else {
-          el.price = km_price;
-        }
-      }
-    });
-  }
+  isSearch: boolean = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSelectModule} from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon";
+import {SearchBarComponent} from "./components/search-bar/search-bar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatBadgeModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
